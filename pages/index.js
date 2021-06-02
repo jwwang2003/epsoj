83fac81d8d98c394cc29a77b7854725ed88a1b50
Jun Wei Wang <jwwang.2003@gmail.com>
2021-06-01 20:09:09 -0700

    minor

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import { Layout, Typography, Form, Input, Button, Checkbox, Spin } from "antd";
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import jwt from 'jsonwebtoken';
 
 const { Footer, Content } = Layout;
 const { Text, Link, Paragraph } = Typography;
@@ -12,7 +15,47 @@ const styleLogin = {
   justifyContent: "center"
 }
 
-export default function Home() {
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+  },
+}
+
+export async function getServerSideProps({ req, res }) {
+  // Get the user's session based on the request
+  // res.cookie('cookieName','allahu akbar', { maxAge: 1000, httpOnly: false });
+  
+  console.log(req.cookies);
+
+  if (req.cookies.auth) {
+    const token = await jwt.decode(JSON.parse(req.cookies.auth), {json: true});
+    console.log(token)
+    if (token.type === "admin") {
+      return {
+        redirect: {
+          destination: '/admin',
+          permanent: false,
+        },
+      }
+    } else {
+      return {
+        redirect: {
+          destination: '/app',
+          permanent: false,
+        },
+      }
+    }
+  } else {
+    return {
+      props: {}
+    }
+  }
+}
+
+export default function Home(props) {
+  console.log(props.child)
   return (
     <Layout className="bigBox">
       <Content style={{ position: "relative" }}>
@@ -38,8 +81,33 @@ export default function Home() {
 }
 
 function Login() {
+  const router = useRouter();
+
   const onFinish = (values) => {
     console.log("Success:", values);
+    axios('http://localhost:8000/auth', {
+      method: "post",
+      data: {
+        username: values.studentID, 
+        password: values.password,
+        remember: values.remember
+      },
+      withCredentials: true,
+    })
+      .then((response) => {
+        console.log(response.data);
+        const { OK, result, type } = response.data;
+        
+
+        if(OK) {
+          if(type === "admin") {
+            router.push("/admin/recents")
+          }
+        }
+
+      }, (error) => {
+        console.log(error);
+      });
   };
 
   return (
